8f00c87394b2ca223bc9650eafe1ae3f4ed23c35
Yousssefch <youssefchakir003@gmail.com>
2025-03-05 12:24:44 -0500

    Added Navbar

---
+++ e-hotel/src/app/pages/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import "./Homepage.css"
+import NavBar from "@/app/components/global/Navbar"
+export default function Homepage(){
+    return(
+        <div className= "Container">
+
+            {/* NavBar */}
+            <NavBar />
+            
+        </div>
+    )
+}
