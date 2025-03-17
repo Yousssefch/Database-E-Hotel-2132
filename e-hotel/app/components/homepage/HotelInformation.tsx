fc7c552e9b0d431361860ffbd9269bc3bca89c41
Yousssefch <youssefchakir003@gmail.com>
2025-03-17 03:14:15 -0400

    fixed hotel information component

--- e-hotel/app/components/homepage/HotelInformation.tsx
+++ e-hotel/app/components/homepage/HotelInformation.tsx
@@ -16,7 +16,7 @@ export default function HotelInformation({ isOpen }: HotelInformationProps) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-80">
      <div className="hotel-information-overlay" />
-      <div className="bg-white rounded-2xl shadow-lg max-w-2xl w-full p-6 relative">
+      <div className="bg-white rounded-2xl shadow-lg w-3/4 h-1/2 p-6 relative">
         <div className="flex">
           {/* Hotel Image */}
           <div className="w-1/3">
@@ -30,14 +30,14 @@ export default function HotelInformation({ isOpen }: HotelInformationProps) {
           {/* Hotel Details */}
           <div className="w-2/3 pl-6 flex flex-col justify-between">
             <div>
-              <h2 className="text-2xl font-bold">Hotel of Paris</h2>
+              <h2 className="text-2xl font-bold text-black">Hotel of Paris</h2>
               <p className="text-gray-600">Paris, France</p>
             </div>
 
             {/* Date Selection */}
             <div className="mt-4 space-y-3">
-              <label className="flex items-center gap-2">
-                <CalendarIcon className="w-5 h-5" /> Start Date
+              <label className="flex items-center gap-2 text-black">
+                <CalendarIcon className="w-5 h-5 text-black"  /> Start Date
                 <input
                   type="date"
                   value={startDate}
@@ -56,6 +56,26 @@ export default function HotelInformation({ isOpen }: HotelInformationProps) {
               </label>
             </div>
 
+            {/* Additional Informations */}
+            <div className="text-black">
+                <h1 className="hotel-information-add-info">Additional Information :</h1>
+
+                <div className="flex">
+                  <label className="text-xs font-semibold">Address:</label>
+                  <h1 className="text-xs ml-1 font-light">Text</h1>
+                </div>
+
+                <div className="flex">
+                  <label className="text-xs font-semibold">Phone number:</label>
+                  <h1 className="text-xs ml-1 font-light">Text</h1>
+                </div>
+
+                <div className="flex">
+                  <label className="text-xs font-semibold">Price:</label>
+                  <h1 className="text-xs ml-1 font-light">Text</h1>
+                </div>
+            </div>
+
             {/* Reserve Button */}
             <div className="flex justify-end mt-4">
               <button className="bg-black text-white px-6 py-2 rounded-lg">
@@ -63,6 +83,11 @@ export default function HotelInformation({ isOpen }: HotelInformationProps) {
               </button>
             </div>
           </div>
+        </div>
+        <div className="absolute top-0 right-1">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+          </svg>
         </div>
       </div>
     </div>
--- e-hotel/app/homepage/page.tsx
+++ e-hotel/app/homepage/page.tsx
@@ -3,9 +3,10 @@ import Navbar from "@/app/components/global/Navbar"
 import topImage from "../../../assets/Homepage/homepage_image.png"
 import HotelCard from "../components/homepage/HotelCard";
 import HotelInformation from "../components/homepage/HotelInformation";
+
 const Homepage: React.FC = () =>  {
     return(
-        <div className= "homepage-container">
+        <div className= "homepage-container bg-white">
             <Navbar />
             
             <div className="homepage-right-container">
@@ -19,19 +20,32 @@ const Homepage: React.FC = () =>  {
                     </div>
                 </div>
 
-                
-
                 {/* Hotel List */}
                 <div className="homepage-hotel-text-container">
                     <h1 className="homepage-hotel-text-title text-black">Hotels</h1>
                     <h2 className="homepage-hotel-text-subtitle">Choose which Hotel you want to spend your days in</h2>
                 </div>
 
-                {/* search bar and shinanigans */}
-                <div className="flex w-full flex-wrap md:flex-nowrap">
+                {/* Search bar */}
+                <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
                     <div>
                         <input type="text" id="first_name" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 homepage-input" placeholder="Search" required />
                     </div>
+                </div>
+
+                {/* Sorting mechanics */}
+                <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4 homepage-sort-container">
+                    <h1 className="text-black">Sort:</h1>
+                    <input type="number" placeholder="Min Price" className="border border-gray-300 text-black text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 bg-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 homepage-sort" />
+                    <input type="date" placeholder="Start Date Available" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 bg-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-gray-500 dark:focus:border-gray-500 homepage-sort" />
+                    <select className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 bg-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 homepage-sort">
+                        <option value="all">All Areas</option>
+                        <option value="new_york">New York</option>
+                        <option value="london">London</option>
+                        <option value="paris">Paris</option>
+                        <option value="vancouver">Vancouver</option>
+                        <option value="toronto">Toronto</option>
+                    </select>
                 </div>
 
                 <div>
