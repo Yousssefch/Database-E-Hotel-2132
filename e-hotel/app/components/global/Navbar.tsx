5da14548e56ae92fa489df0ead816e8ff7c43e47
Yousssefch <youssefchakir003@gmail.com>
2025-03-17 00:37:55 -0400

    re-added Navbar

---
+++ e-hotel/app/components/global/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Navbar.css";
+
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar-container">
+    <div className="navbar">
+      <img src="images/Logo.png" className="navbar-img-logo" />
+      <div className="navbar-logo">
+        
+      </div>
+
+      {/* Menu Items */}
+      <nav className="navbar-menu">
+        <a href="#" className="navbar-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+        </svg>
+          <span className="navbar-text">Browse</span>
+        </a>
+
+        <a href="#" className="navbar-item">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+         </svg>
+          <span className="navbar-text">My Reservations</span>
+        </a>
+
+        <a href="#" className="navbar-item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg>
+          <span className="navbar-text">Account</span>
+        </a>
+      </nav>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ e-hotel/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/global/Navbar"
+
+const HomePage: React.FC = () => {
+    return(
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+export default HomePage;
